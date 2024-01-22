40ffc1aa93ba9b10de74124ca565ffeed16b4755
aizaada <akylbekova.aizaada@gmail.com>
2024-01-22 16:57:13 +0600

    explanation

---
+++ app.js
@@ -0,0 +1,11 @@
+import {getItems, countItems, studentAge as age, studentName} from "./items.js";
+// import * as items from "./items.js";
+
+const sandar=[3,5,22,14, 88]
+
+
+
+items.countItems(sandar)
+
+console.log(items.getItems(5,4));
+console.log(items.studentAge, items.studentName);
---
+++ items.js
@@ -0,0 +1,20 @@
+const fns={
+    getItems: function(a,b){
+        return a+b
+    }, 
+    countItems: function (array){
+        const summa=array.reduce((total, el)=>total+el,0)
+        
+        console.log(summa);
+    }
+}
+
+export const studentName='Amina'
+export const studentAge=15
+
+
+
+export const getItems =fns.getItems
+export const countItems =fns.countItems
+
+
